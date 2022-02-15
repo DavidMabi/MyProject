2981e673edb73ca2981a94b344892dd6d5580be9
Avi <96371542+DavidMabi@users.noreply.github.com>
2022-02-15 11:07:31 +0800

    Add files via upload

---
+++ Proj/Python/condition.py
@@ -0,0 +1,4 @@
+n=int (input("Number: "))
+
+if n>0:
+    print("positive")
---
+++ Proj/Python/hello.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+strA = name
+if (not strA):
+        print("Enter your name pls")
+        
+else:
+
+    print(f"Hello, {name}")
